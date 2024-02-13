2877066be30564bcaadc2796fa4eddbf09b48482
ROHIT KUMAR <105981384+geniusrks1@users.noreply.github.com>
2024-02-13 09:15:38 +0530

    Create Binary Search.java
    Problem statement
    You are given an integer array 'A' of size 'N', sorted in non-decreasing order. You are also given an integer 'target'.
    
    
    
    Your task is to write a function to search for 'target' in the array 'A'. If it exists, return its index in 0-based indexing. If 'target' is not present in the array 'A', return -1.
    
    
    
    Note:
    
    You must write an algorithm whose time complexity is O(LogN)
    
    
    Example:
    
    
    Input: ‘N’ = 7 ‘target’ = 3
    ‘A’ = [1, 3, 7, 9, 11, 12, 45]
    
    Output: 1
    
    Explanation: A = [1, 3, 7, 9, 11, 12, 45],
    The index of element '3' is 1.
    Hence, the answer is '1'.
    Detailed explanation ( Input/output format, Notes, Images )
    Constraints :
    1 <= N <= 10^5
    1 <= A[i] <= 10^9
    1 <= target <= 10^9
    Time Limit: 1 sec
    Sample Input 1:
    7
    1 3 7 9 11 12 45
    3
    Sample Output 1:
    1
    Explanation of sample output 1:
    nums = [1, 3, 7, 9, 11, 12, 45],
    The index of element '3' is 1.
    Hence, the answer is '1'.
    
    
    Sample Input 2:
    7
    1 2 3 4 5 6 7
    9
    Sample Output 2:
    -1
    Explanation of sample output 2:
    nums = [1, 2, 3, 4, 5, 6, 7],
    Element '9' doesn't exist.
    Hence, the answer is '-1'.

---
+++ Recursion/Binary Search.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+    public static int search(int[] nums, int target) {
+        return binarySearch(nums, target, 0, nums.length - 1);
+    }
+
+    private static int binarySearch(int[] nums, int target, int left, int right) {
+        if (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;  
+            } else if (nums[mid] < target) {
+                return binarySearch(nums, target, mid + 1, right);  
+            } else {
+                return binarySearch(nums, target, left, mid - 1); 
+            }
+        }
+
+        return -1;
+    }
+
+
+}
